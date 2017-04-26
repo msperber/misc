34fa0edde2ef527338efd16145497c8537ebeb15
Matthias Sperber <matthias.sperber@gmail.com>
2017-04-26 07:07:21 +0200

    started implementing conv-lstm

--- xnmt/lstm.py
+++ xnmt/lstm.py
@@ -1,4 +1,5 @@
 import dynet as dy
+from embedder import TensorExprEmbeddedSentence
 
 class PythonLSTMBuilder:
   """
@@ -26,6 +27,80 @@ class PythonLSTMBuilder:
   def add_inputs(self):
     pass
   def transduce(self, xs):
+    i_x2i = dy.parameter(self.p_x2i)
+    i_h2i = dy.parameter(self.p_h2i)
+    i_bi = dy.parameter(self.p_bi)
+    h = []
+    c = []
+    for i, x_t in enumerate(xs):
+      if i==0:
+        tmp = dy.affine_transform([i_bi, i_x2i, x_t])
+      else:
+        tmp = dy.affine_transform([i_bi, i_x2i, x_t, i_h2i, h[-1]])
+      i_ait = dy.pickrange(tmp, 0, self.hidden_dim)
+      i_aft = dy.pickrange(tmp, self.hidden_dim, self.hidden_dim*2)
+      i_aot = dy.pickrange(tmp, self.hidden_dim*2, self.hidden_dim*3)
+      i_agt = dy.pickrange(tmp, self.hidden_dim*3, self.hidden_dim*4)
+      i_it = dy.logistic(i_ait)
+      i_ft = dy.logistic(i_aft + 1.0)
+      i_ot = dy.logistic(i_aot)
+      i_gt = dy.tanh(i_agt)
+      if i==0:
+        c.append(dy.cmult(i_it, i_gt))
+      else:
+        c.append(dy.cmult(i_ft, c[-1]) + dy.cmult(i_it, i_gt))
+      h.append(dy.cmult(i_ot, dy.tanh(c[-1])))
+    return h
+  
+class ConvLSTMBuilder:
+  """
+  This is a ConvLSTM implementation for a single layer & direction.
+  """
+  def __init__(self, layers, input_dim, hidden_dim, model, chn_dim=3):
+    if layers!=1: raise RuntimeError("ConfLSTMBuilder supports only exactly one layer")
+    self.input_dim = input_dim
+    self.hidden_dim = hidden_dim
+  
+    # [i; f; o; g]
+    self.p_x2i = model.add_parameters(dim=(hidden_dim*4, input_dim)) # Ws
+    self.p_h2i = model.add_parameters(dim=(hidden_dim*4, hidden_dim)) # Wfs
+    self.p_bi  = model.add_parameters(dim=(hidden_dim*4,), init=dy.ConstInitializer(0.0)) # Ufs
+
+    self.chn_dim = chn_dim
+    self.freq_dim = input_dim / chn_dim
+    self.num_filters = 32
+    self.filter_size_time = 1
+    self.filter_size_freq = 3
+    normalInit=dy.NormalInitializer(0, 0.1)
+    self.filters_vertical = model.add_parameters(dim=(self.filter_size_time, self.filter_size_freq, self.chn_dim, self.num_filters),
+                                         init=normalInit)
+    self.filters_horizontal = model.add_parameters(dim=(self.filter_size_time, self.filter_size_freq, self.chn_dim, self.num_filters),
+                                         init=normalInit)
+    
+  def whoami(self): return "ConvLSTMBuilder"
+  
+  def set_dropout(self, p):
+    if p>0.0: raise RuntimeError("ConvLSTMBuilder does not support dropout")
+  def disable_dropout(self):
+    pass
+  def initial_state(self):
+    return self
+  def add_inputs(self):
+    pass
+  def transduce(self, es):
+    if isinstance(es, TensorExprEmbeddedSentence):
+      es_expr = es.get_tensor_repr()
+    else:
+      es_expr = dy.concatenate(map(lambda v: dy.transpose(v), es))
+    sent_len = es_expr.dim()[0][0]
+    batch_size=es_expr.dim()[1]
+    
+    es_chn = dy.reshape(es_expr, (sent_len, self.freq_dim, self.chn_dim), batch_size=batch_size) # ((276, 80, 3), 1)
+#    cnn_layer1 = dy.conv2d(es_chn, dy.parameter(self.filters1), stride=self.stride, is_valid=True) # ((137, 39, 32), 1)
+#    cnn_layer2 = dy.conv2d(cnn_layer1, dy.parameter(self.filters2), stride=self.stride, is_valid=True) # ((68, 19, 32), 1)
+#    cnn_out = dy.reshape(cnn_layer2, (cnn_layer2.dim()[0][0], cnn_layer2.dim()[0][1]*cnn_layer2.dim()[0][2]), batch_size=batch_size) # ((68, 608), 1)
+#    es_list = [cnn_out[i] for i in range(cnn_out.dim()[0][0])]
+
     i_x2i = dy.parameter(self.p_x2i)
     i_h2i = dy.parameter(self.p_h2i)
     i_bi = dy.parameter(self.p_bi)
