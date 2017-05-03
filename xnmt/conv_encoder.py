cefc31994a4b7632913da5e6a8ee6a22ec458a49
Matthias Sperber <matthias.sperber@gmail.com>
2017-05-03 19:55:58 +0200

    some steps toward batch-normalized CNN encoder

--- xnmt/conv_encoder.py
+++ xnmt/conv_encoder.py
@@ -1,5 +1,6 @@
 import math
 import dynet as dy
+import numpy as np
 from residual import PseudoState
 from embedder import ExpressionSequence
 
@@ -104,6 +105,7 @@ class StridedConvEncBuilder(object):
     :param model
     :param chn_dim: channel dimension
     :param num_filters
+    :param output_tensor: if set, the output is directly given as a 3d-tensor, rather than converted to a list of vector expressions
     """
     assert num_layers > 0
     assert input_dim % chn_dim == 0
@@ -117,7 +119,13 @@ class StridedConvEncBuilder(object):
     self.stride = (2,2)
     self.output_tensor = output_tensor
     
+    self.use_bn = True
+    self.bn_eps = 0.00001
+    
     normalInit=dy.NormalInitializer(0, 0.1)
+    self.filters_layers = []
+    self.bn_gamma_layers = []
+    self.bn_beta_layers = []
     self.filters_layers = []
     for layer_i in range(num_layers):
       filters = model.add_parameters(dim=(self.filter_size_time,
@@ -125,7 +133,14 @@ class StridedConvEncBuilder(object):
                                           self.chn_dim if layer_i==0 else self.num_filters,
                                           self.num_filters),
                                      init=normalInit)
+      if self.use_bn:
+        bn_gamma = model.add_parameters(dim=(self.num_filters, ), init=dy.NumpyInitializer(np.random.uniform(0,1,self.num_filters)))
+        bn_beta = model.add_parameters(dim=(self.num_filters, ), init=normalInit)
+      else:
+        bn_gamma, bn_beta = None, None
       self.filters_layers.append(filters)
+      self.bn_gamma_layers.append(bn_gamma)
+      self.bn_beta_layers.append(bn_beta)
   
   def get_output_dim(self):
     conv_dim = self.freq_dim
@@ -160,11 +175,31 @@ class StridedConvEncBuilder(object):
       es_expr = dy.concatenate([es_expr, dy.zeroes((pad_size, self.freq_dim * self.chn_dim), batch_size=es_expr.dim()[1])])
       sent_len += pad_size
 
-    # convolution layers    
+    # loop over layers    
     es_chn = dy.reshape(es_expr, (sent_len, self.freq_dim, self.chn_dim), batch_size=batch_size)
     cnn_layer = es_chn
-    for filters in self.filters_layers:
+    for (filters, bn_gamma, bn_beta) in zip(self.filters_layers, self.bn_gamma_layers, self.bn_beta_layers):
+      # convolution layer
       cnn_layer = dy.conv2d(cnn_layer, dy.parameter(filters), stride=self.stride, is_valid=True)
+      if self.use_bn:
+        param_bn_gamma = dy.parameter(bn_gamma)
+        param_bn_beta = dy.parameter(bn_beta)
+        # TODO: batch normalization layer
+        bn_mean = dy.mean_batches(dy.mean_dim(dy.mean_dim(cnn_layer, 1),0)) # output dim: ((32,), 1)
+        bn_var = dy.moment_batches(dy.moment_dim(dy.moment_dim(cnn_layer, 1, 2),0, 2), 2)
+        bn_per_channel = []
+        for chn_i in range(self.num_filters):
+          bn_chn_num = dy.pick(cnn_layer, chn_i, 2) - dy.pick(bn_mean, chn_i, 0)
+          bn_chn_denom = dy.sqrt(dy.pick(bn_var, chn_i, 0) + self.bn_eps)
+          # TODO: need expr * expr or expr / expr where one expr is a matrix, the other is a scalar
+  #        bn_per_channel.append(bn_chn_num / bn_chn_denom)
+          bn_xhat = bn_chn_num # / bn_chn_denom
+#          bn_y = dy.pick(param_bn_gamma, chn_i) * bn_xhat + dy.pick(param_bn_beta, chn_i) 
+          bn_y = bn_xhat * 0.5 + dy.pick(param_bn_beta, chn_i) 
+          bn_per_channel.append(bn_y)
+          # TODO: handle case of inference
+        bnormalized_cnn_layer = dy.concatenate(bn_per_channel, 2)
+        cnn_layer = bnormalized_cnn_layer
     
     if self.output_tensor:
       return cnn_layer
