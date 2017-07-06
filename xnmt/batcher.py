4e0b2c052aa699f3b0b74a308b010c5673723c2a
Matthias Sperber <matthias.sperber@gmail.com>
2017-07-06 16:29:24 +0200

    print resulting batch size for 'word' batch strategy

--- xnmt/batcher.py
+++ xnmt/batcher.py
@@ -224,6 +224,6 @@ class WordTargetBucketBatcher(TargetBucketBatcher):
 
     if temp_words != 0:
       result.append(self.pad_sent(temp_batch))
-
+    print("WordTargetBucketBatcher avg batch size: %s" % (float(sum([len(x) for x in result]))/len(result)))
     np.random.shuffle(result)
     return self.separate_src_trg(result)
