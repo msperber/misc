0392fcddf61567c2ae7f96ff3b08f29e0abe821c
Matthias Sperber <matthias.sperber@gmail.com>
2017-09-28 16:50:53 +0200

    code style

--- script/code/perturb_seq.py
+++ script/code/perturb_seq.py
@@ -40,11 +40,11 @@ from scipy.special import binom
 from scipy.misc import logsumexp
 
 class Usage(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+  def __init__(self, msg):
+    self.msg = msg
 class ModuleTest(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+  def __init__(self, msg):
+    self.msg = msg
 
 
 class Aligner:
@@ -124,177 +124,175 @@ class Aligner:
 
 
 def sample_corrupted(words, tau, vocab, vocab_weights=None, op_weights=(1,1,1), ignoreVocab=[], use_word_sim=False):
-    sent_len = len([w for w in words if not w in ignoreVocab])
-    distance = sample_edit_distance(tau, sent_len)
-    sub_del_candidate_positions = [i for i in range(len(words)) if not words[i] in ignoreVocab]
-    num_sub, num_ins, num_del = sample_num_operations(distance, op_weights)
-    sub_positions = sample_sub_positions(sub_del_candidate_positions, num_sub)
-    del_positions = sample_sub_positions([p for p in sub_del_candidate_positions if not p in sub_positions], num_del)
-    ins_positions = sample_ins_positions(range(sent_len+1), num_ins)
-    ret_words = \
-        corrupt_positions(words, vocab, sub_positions, ins_positions, del_positions, 
-                          vocab_weights=vocab_weights, use_word_sim=use_word_sim)
-    return ret_words, num_sub, num_ins, num_del
+  sent_len = len([w for w in words if not w in ignoreVocab])
+  distance = sample_edit_distance(tau, sent_len)
+  sub_del_candidate_positions = [i for i in range(len(words)) if not words[i] in ignoreVocab]
+  num_sub, num_ins, num_del = sample_num_operations(distance, op_weights)
+  sub_positions = sample_sub_positions(sub_del_candidate_positions, num_sub)
+  del_positions = sample_sub_positions([p for p in sub_del_candidate_positions if not p in sub_positions], num_del)
+  ins_positions = sample_ins_positions(range(sent_len+1), num_ins)
+  ret_words = \
+      corrupt_positions(words, vocab, sub_positions, ins_positions, del_positions, 
+                        vocab_weights=vocab_weights, use_word_sim=use_word_sim)
+  return ret_words, num_sub, num_ins, num_del
 
 def sample_edit_distance(tau, sent_len):
-    lam = tau * sent_len
-    sampled = None
-    #return min(np.random.poisson(lam=lam), sent_len)
-    while sampled is None or sampled > sent_len:
-      sampled = np.random.poisson(lam=lam)
-    return sampled
+  lam = tau * sent_len
+  sampled = None
+  while sampled is None or sampled > sent_len: # TODO: would be nicer to compute truncated distribution explicitly, but this will do for now
+    sampled = np.random.poisson(lam=lam)
+  return sampled
 
 def sample_num_operations(distance, op_weights):
-    num_buckets = sum(op_weights)
-    sorted_samples = sorted(np.random.choice(range(1,distance+num_buckets), size=num_buckets-1, replace=False))
-    sorted_samples = [0] + sorted_samples + [distance+num_buckets]
-    num_sub = sum([sorted_samples[i+1] - sorted_samples[i] - 1 for i in range(0,op_weights[0])])
-    num_ins = sum([sorted_samples[i+1] - sorted_samples[i] - 1 for i in range(op_weights[0],op_weights[0]+op_weights[1])])
-    num_del = sum([sorted_samples[i+1] - sorted_samples[i] - 1 for i in range(op_weights[0]+op_weights[1],op_weights[0]+op_weights[1]+op_weights[2])])
-    assert distance == num_sub + num_ins + num_del
-    assert min(num_sub, num_ins, num_del) >= 0
-    return num_sub, num_ins, num_del
+  num_buckets = sum(op_weights)
+  sorted_samples = sorted(np.random.choice(range(1,distance+num_buckets), size=num_buckets-1, replace=False))
+  sorted_samples = [0] + sorted_samples + [distance+num_buckets]
+  num_sub = sum([sorted_samples[i+1] - sorted_samples[i] - 1 for i in range(0,op_weights[0])])
+  num_ins = sum([sorted_samples[i+1] - sorted_samples[i] - 1 for i in range(op_weights[0],op_weights[0]+op_weights[1])])
+  num_del = sum([sorted_samples[i+1] - sorted_samples[i] - 1 for i in range(op_weights[0]+op_weights[1],op_weights[0]+op_weights[1]+op_weights[2])])
+  assert distance == num_sub + num_ins + num_del
+  assert min(num_sub, num_ins, num_del) >= 0
+  return num_sub, num_ins, num_del
 
 def sample_sub_positions(pos_choices, num_sub):
-    if num_sub==0: return []
-    sub_positions = np.random.choice(pos_choices, size=num_sub, replace=False)
-    return sub_positions
+  if num_sub==0: return []
+  sub_positions = np.random.choice(pos_choices, size=num_sub, replace=False)
+  return sub_positions
 
 def sample_ins_positions(pos_choices, num_ins):
-    if num_ins==0: return []
-    ins_positions = np.random.choice(a=pos_choices, size=num_ins, replace=True)
-    return ins_positions
+  if num_ins==0: return []
+  ins_positions = np.random.choice(a=pos_choices, size=num_ins, replace=True)
+  return ins_positions
 
 def corrupt_positions(words, vocab, sub_positions, ins_positions, del_positions,
-                      vocab_weights=None, use_word_sim=False):
-    if use_word_sim: assert vocab_weights is None
+                    vocab_weights=None, use_word_sim=False):
+  if use_word_sim: assert vocab_weights is None
     
-    ret_words = list(words)
-    for pos in sub_positions:
-        word = words[pos]
-        if use_word_sim:
-            vocab_weights = get_similarities(word, vocab)
-        while(word==words[pos]):
-            word = np.random.choice(vocab, p=vocab_weights)
-        ret_words[pos] = word
-    for pos in del_positions:
-        ret_words[pos] = None
-    for pos in reversed(sorted(ins_positions)):
-        if use_word_sim:
-            vocab_weights = get_similarities("", vocab)
-        word = np.random.choice(vocab, p=vocab_weights)
-        ret_words.insert(pos, word)
-    ret_words = [w for w in ret_words if w is not None]
-    return ret_words
+  ret_words = list(words)
+  for pos in sub_positions:
+    word = words[pos]
+    if use_word_sim:
+      vocab_weights = get_similarities(word, vocab)
+    while(word==words[pos]):
+      word = np.random.choice(vocab, p=vocab_weights)
+    ret_words[pos] = word
+  for pos in del_positions:
+    ret_words[pos] = None
+  for pos in reversed(sorted(ins_positions)):
+    if use_word_sim:
+      vocab_weights = get_similarities("", vocab)
+    word = np.random.choice(vocab, p=vocab_weights)
+    ret_words.insert(pos, word)
+  ret_words = [w for w in ret_words if w is not None]
+  return ret_words
 
 aligner = Aligner()
 cache = {}
 def get_similarities(word, vocab):
-    if word in cache:
-        return cache[word]
-    else:
-        dist = [aligner.align(word, w2)[0] for w2 in vocab]
-        similarities=np.exp(dist)/sum(np.exp(dist))
-        if len(cache) < 1000:
-            cache[word] = similarities
-        return similarities
+  if word in cache:
+    return cache[word]
+  else:
+    dist = [aligner.align(word, w2)[0] for w2 in vocab]
+    similarities=np.exp(dist)/sum(np.exp(dist))
+    if len(cache) < 1000:
+      cache[word] = similarities
+    return similarities
 
 def main(argv=None):
-    arguments = docopt.docopt(__doc__, options_first=True, argv=argv)
-    print arguments
+  arguments = docopt.docopt(__doc__, options_first=True, argv=argv)
+  print arguments
     
-    tau = float(arguments["<tau>"])
-    inputFileName = arguments["<in.txt>"]
-    outputFileName = arguments["<out.txt>"]
-    if outputFileName:
-        outF = io.open(outputFileName + ".tmp", "w")
+  tau = float(arguments["<tau>"])
+  inputFileName = arguments["<in.txt>"]
+  outputFileName = arguments["<out.txt>"]
+  if outputFileName:
+    outF = io.open(outputFileName + ".tmp", "w")
     
-    emptyLineRepl = arguments["--empty-line-repl"]
-    if emptyLineRepl is None: emptyLineRepl = ""
+  emptyLineRepl = arguments["--empty-line-repl"]
+  if emptyLineRepl is None: emptyLineRepl = ""
     
-    op_weights = arguments["--op-weights"]
-    if op_weights is None: op_weights = (1,1,1)
-    else: op_weights = tuple([int(w) for w in op_weights.split()])
+  op_weights = arguments["--op-weights"]
+  if op_weights is None: op_weights = (1,1,1)
+  else: op_weights = tuple([int(w) for w in op_weights.split()])
     
-    vocabFileName = arguments["--vocab"]
-    weightedVocabFileName = arguments["--weighted-vocab"]
+  vocabFileName = arguments["--vocab"]
+  weightedVocabFileName = arguments["--weighted-vocab"]
 
-    ignoreVocabFileName = arguments["--ignore-vocab"]
+  ignoreVocabFileName = arguments["--ignore-vocab"]
+   
+  assumeCharTokens = arguments["--char-tokens"]
     
-    assumeCharTokens = arguments["--char-tokens"]
-    
-    vocab = []
-    vocab_weights = None
-    if vocabFileName:
-        for v in io.open(vocabFileName).readlines():
-            vocab.append(v.strip())
-    elif weightedVocabFileName:
-        vocab_weights = []
-        for line in io.open(weightedVocabFileName).readlines():
-            vocab.append(line.split()[1])
-            vocab_weights.append(float(line.split()[0]))
-        weightSum = sum(vocab_weights)
-        for i in range(len(vocab_weights)):
-            vocab_weights[i] /= weightSum
-    else:
-        vocabSet = set()
-        for line in io.open(inputFileName):
-            if assumeCharTokens:
-                line = "".join([c if c!=u"__" else u" " for c in line.split()])
-            for v in line.strip().split():
-                vocabSet.add(v)
-        vocab = list(vocabSet)
-    ignoreVocab = []
-    if ignoreVocabFileName:
-        for v in io.open(ignoreVocabFileName).readlines():
-            ignoreVocab.append(v.strip())
-
-
-    ###########################
-    ## MAIN PROGRAM ###########
-    ###########################
-
-    total_sub, total_ins, total_del, total_ref_len = 0, 0, 0, 0
+  vocab = []
+  vocab_weights = None
+  if vocabFileName:
+    for v in io.open(vocabFileName).readlines():
+      vocab.append(v.strip())
+  elif weightedVocabFileName:
+    vocab_weights = []
+    for line in io.open(weightedVocabFileName).readlines():
+      vocab.append(line.split()[1])
+      vocab_weights.append(float(line.split()[0]))
+    weightSum = sum(vocab_weights)
+    for i in range(len(vocab_weights)):
+      vocab_weights[i] /= weightSum
+  else:
+    vocabSet = set()
     for line in io.open(inputFileName):
-        if assumeCharTokens:
-            line = u"".join([c if c!=u"__" else u" " for c in line.split()])
-        words = line.strip().split()
-        ret_words, num_sub, num_ins, num_del = \
-                        sample_corrupted(
-                                        words=words, 
-                                        tau=tau, vocab=vocab,
-                                        vocab_weights=vocab_weights,
-                                        op_weights=op_weights,
-                                        ignoreVocab=ignoreVocab)
-        total_sub += num_sub
-        total_ins += num_ins
-        total_del += num_del
-        total_ref_len += len(words)
-        outLine = u" ".join(ret_words)
-        
-        if assumeCharTokens:
-            outLine = u" ".join([s if s!=u" " else u"__" for s in list(outLine)])
-        
-        if len(ret_words)==0:
-            outLine = emptyLineRepl
+      if assumeCharTokens:
+        line = "".join([c if c!=u"__" else u" " for c in line.split()])
+      for v in line.strip().split():
+        vocabSet.add(v)
+    vocab = list(vocabSet)
+  ignoreVocab = []
+  if ignoreVocabFileName:
+    for v in io.open(ignoreVocabFileName).readlines():
+      ignoreVocab.append(v.strip())
 
-        if outputFileName is not None:
-            outF.write((outLine + u"\n"))
-        else:
-            print outLine
-    
-    print >> sys.stderr, "=========="
-    print >> sys.stderr, "average edit rate:", \
-            float(total_sub+total_ins+total_del)/float(total_ref_len)*100.0, \
-            "% (S:", total_sub, ", I:", total_ins, ", D:", total_del, ")"
+
+  ###########################
+  ## MAIN PROGRAM ###########
+  ###########################
+
+  total_sub, total_ins, total_del, total_ref_len = 0, 0, 0, 0
+  for line in io.open(inputFileName):
+    if assumeCharTokens:
+      line = u"".join([c if c!=u"__" else u" " for c in line.split()])
+    words = line.strip().split()
+    ret_words, num_sub, num_ins, num_del = \
+                    sample_corrupted(words=words, 
+                                     tau=tau, vocab=vocab,
+                                     vocab_weights=vocab_weights,
+                                     op_weights=op_weights,
+                                     ignoreVocab=ignoreVocab)
+    total_sub += num_sub
+    total_ins += num_ins
+    total_del += num_del
+    total_ref_len += len(words)
+    outLine = u" ".join(ret_words)
+        
+    if assumeCharTokens:
+      outLine = u" ".join([s if s!=u" " else u"__" for s in list(outLine)])
+       
+    if len(ret_words)==0:
+      outLine = emptyLineRepl
+
     if outputFileName is not None:
-        outF.close()
-        os.rename(outputFileName + ".tmp", outputFileName)
+      outF.write((outLine + u"\n"))
+    else:
+      print outLine
+  
+  print >> sys.stderr, "=========="
+  print >> sys.stderr, "average edit rate:", \
+          float(total_sub+total_ins+total_del)/float(total_ref_len)*100.0, \
+          "% (S:", total_sub, ", I:", total_ins, ", D:", total_del, ")"
+  if outputFileName is not None:
+    outF.close()
+    os.rename(outputFileName + ".tmp", outputFileName)
 
 
-    ###########################
-    ###########################
+  ###########################
+  ###########################
 
 if __name__ == "__main__":
-    sys.exit(main())
+  sys.exit(main())
     
